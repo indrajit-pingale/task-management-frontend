5c1f5985e1c140bf907ceea63097cfa09560f36d
indrajit-pingale <indrajit-pingale@users.noreply.github.com>
2023-10-21 15:37:53 +0000

    deploy: 64bf8836b0fd6ef6e97e3e09239af3eb16660013

---
+++ precache-manifest.4b6a5cb458ac8d3557e46184523923d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40536bea2cb634c98d7fd092444ba303",
+    "url": "https://indrajit-pingale.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "1006d9bc49c0c84d6d7b",
+    "url": "https://indrajit-pingale.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "b8a49f0b5066e9fecd0b",
+    "url": "https://indrajit-pingale.github.io/task-management-frontend/static/js/2.8079b2e4.chunk.js"
+  },
+  {
+    "revision": "1006d9bc49c0c84d6d7b",
+    "url": "https://indrajit-pingale.github.io/task-management-frontend/static/js/main.fcfb6047.chunk.js"
+  },
+  {
+    "revision": "0447fcf9615e7cc33f25",
+    "url": "https://indrajit-pingale.github.io/task-management-frontend/static/js/runtime~main.036ead54.js"
+  }
+]);
